8ea2207c40511b2da71b0d6de1b810aa54155b9c
Adrian Kurt <dilanmarinas@gmail.com>
2025-03-26 17:16:27 +0800

    the while condition is not correct, it removes all characters

--- not_using_lstrip().py
+++ not_using_lstrip().py
@@ -6,5 +6,6 @@ string = input("Input a statement: ")
 # check if the first character is a space then remove it
 while string[0] == " ":
     string = string[0:]
-    
+
 # print the string without space at the beginning
+print(string)
