c520c551629721e7186ca689f20fcdb42d424b26
Adrian Kurt <dilanmarinas@gmail.com>
2025-03-28 01:12:06 +0800

    start prog 4 and peudocode

---
+++ not_using_isupper().py
@@ -0,0 +1,7 @@
+# Prog04. isupper() check if all characters of the string is on upper case. Create a program that do the same functionality without using isupper() function.
+
+# prompt the user to input a string
+
+# check if all characters of the string is on upper case
+
+# print the result
