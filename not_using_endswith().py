de2ee9574884db1f507c3ecb75b74ed6f0486d75
Adrian Kurt <dilanmarinas@gmail.com>
2025-03-28 01:21:21 +0800

    start prog 5 and pseudocode

---
+++ not_using_endswith().py
@@ -0,0 +1,7 @@
+# Prog05. endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
+
+# prompt the user to input a string
+
+# check if the end matches the parameter
+
+# print the result
