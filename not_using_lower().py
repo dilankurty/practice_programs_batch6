9266727659d707f0eec31501e4615ae948afe820
Adrian Kurt <dilanmarinas@gmail.com>
2025-03-28 00:55:16 +0800

    added input and tranlation variable

--- not_using_lower().py
+++ not_using_lower().py
@@ -1,9 +1,12 @@
 # Prog03. lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
 
 # prompt the user to input a string
+string = input("Input a string: ")
+
+# create a translation table
+translation = str.maketrans("ABCDEFGHIJKLMNÑOPQRSTUVWXYZ", "abcdefghijklmnñopqrstuvwxyz")
 
 # convert the string to lower case using translation table method
-
 
 # print the string in lower case
 
