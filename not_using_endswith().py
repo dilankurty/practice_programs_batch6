028e2e69afa85be5b05b5ff2ab20368ca31893ca
Adrian Kurt <dilanmarinas@gmail.com>
2025-03-28 01:21:55 +0800

    added input

--- not_using_endswith().py
+++ not_using_endswith().py
@@ -1,6 +1,7 @@
 # Prog05. endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
 
 # prompt the user to input a string
+string = input("Input a string: ")
 
 # check if the end matches the parameter
 
