4d85ae792d1f157fc5f858aded74a2869cf57fc2
Adrian Kurt <dilanmarinas@gmail.com>
2025-03-28 00:21:22 +0800

    added input to prompt user

--- not_using_removeprefix().py
+++ not_using_removeprefix().py
@@ -1,6 +1,7 @@
 # Prog02. removeprefix() remove the characters at the beginning of the string that matches the function parameter. Create a program that do the same functionality without using removeprefix() function.
 
 # prompt the user to input a string
+string = input("Input a string: ")
 
 # remove prefix from the string
 
