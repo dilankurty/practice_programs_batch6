d76ce7b0d34e5c34ea6f2c17d2f572b098160984
Kurt <dilanmarinas@gmail.com>
2025-03-29 00:37:33 +0800

    start prog 8 and pseudocode

---
+++ not_using_swapcase().py
@@ -0,0 +1,9 @@
+# Prog08. swapcase() reverse the casing of each of the character of the string. Create a program that do the same functionality without using swapcase() function.
+
+# prompt the user to input a string
+
+# create a translation table
+
+# using for loop, iterate through the string to tramslate each character
+
+# print the result
