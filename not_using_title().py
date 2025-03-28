81af0c42c4c6f1b90a5320bdc744bb5435154a49
Kurt <dilanmarinas@gmail.com>
2025-03-29 00:58:11 +0800

    start prog 10 and pseudocode

---
+++ not_using_title().py
@@ -0,0 +1,7 @@
+# Prog10. title() makes all first letter of each word in the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using title() function.
+
+# prompt the user to input a string
+
+# converts the first character to uppercase and converts the rest of the characters to lowercase
+
+# print the result
