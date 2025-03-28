fad60a16ae99394009ff625afd7e91665b511c39
Kurt <dilanmarinas@gmail.com>
2025-03-29 00:32:40 +0800

    added left and right spaces to the string

--- not_using_center().py
+++ not_using_center().py
@@ -1,7 +1,10 @@
 # Prog07. center() add space characters at the beginning and at the end of the string to print the string at the center. Create a program that do the same functionality without using center() function.
 
 # prompt the user to input a string
+string = input("Input a string: ")
 
 # add left and right spaces to the string
+string = " " * 50 + string + " " * 50
 
 # print the result
+print(string)
