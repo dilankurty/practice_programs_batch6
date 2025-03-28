9d0e1885ace1c95bd852a8bc36daaaf8e492ee90
Kurt <dilanmarinas@gmail.com>
2025-03-29 00:43:29 +0800

    added input and translation table

--- not_using_swapcase().py
+++ not_using_swapcase().py
@@ -1,8 +1,10 @@
 # Prog08. swapcase() reverse the casing of each of the character of the string. Create a program that do the same functionality without using swapcase() function.
 
 # prompt the user to input a string
+string = input("Input a string: ")
 
 # create a translation table
+translation_table = str.maketrans("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")
 
 # using for loop, iterate through the string to tramslate each character
 
