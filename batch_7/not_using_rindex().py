a10335cc63fee54534ca333e178f9434c7e01b88
Kurt <dilanmarinas@gmail.com>
2025-03-29 15:42:03 +0800

    start prog 10 and pseudocode

---
+++ batch_7/not_using_rindex().py
@@ -0,0 +1,7 @@
+# Prog10. rindex() return the first location of the function parameter in the string starting from the last character. Create a program that do the same functionality without using rindex() function.
+
+# prompt the user to input a string
+
+# check the location of the parameter "a" in the string starting from the last character
+
+# print the result
