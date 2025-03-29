e38db49c9050a6a3ac9f6018bca6292fe5870b1a
Kurt <dilanmarinas@gmail.com>
2025-03-29 14:56:29 +0800

    added multiple inputs using while loop

--- batch_7/not_using_rjust().py
+++ batch_7/not_using_rjust().py
@@ -1,6 +1,12 @@
 # Prog06. rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using rjust() function.
 
 # prompt the user to input a multiple line string
+string_list = []
+
+while True:
+    string = input("Input a string(press enter to stop): ")
+    if string == "":
+        break
 
 # add left spaces to the string
 
