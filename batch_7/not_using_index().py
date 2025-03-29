3d5e5a486da97a6514da5e1eeca7d66381251816
Kurt <dilanmarinas@gmail.com>
2025-03-29 15:39:11 +0800

    correct pseudocode

--- batch_7/not_using_index().py
+++ batch_7/not_using_index().py
@@ -3,7 +3,7 @@
 # prompt the user to input a string
 string = input("Input a string: ")
 
-# check the location of the parameter in the string
+# check the location of the parameter "a" in the string
 for i in range(len(string)):
     if string[i] == "a":
         index = i
