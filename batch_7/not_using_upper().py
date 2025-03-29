616f0e7bc0db3196591f523730f094d75d673b2e
Kurt <dilanmarinas@gmail.com>
2025-03-29 14:20:58 +0800

    added input

--- batch_7/not_using_upper().py
+++ batch_7/not_using_upper().py
@@ -1,6 +1,7 @@
 # Prog03. upper() converts all characters of the string into upper case. Create a program that do the same functionality without using upper() function.
 
 # prompt the user to input a string
+string = input("Input a string: ")
 
 # create a translation table
 
