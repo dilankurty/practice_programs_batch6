a6a83b1ee040361523dbc8a8e41ad41e79ea15c0
Kurt <dilanmarinas@gmail.com>
2025-03-29 14:00:44 +0800

    start prog 1 and pseudocode

---
+++ batch_7/not_using_rstrip().py
@@ -0,0 +1,7 @@
+# Prog01. rstrip() remove the space characters at the end of the string. Create a program that do the same functionality without using rstrip() function.
+
+# prompt the user to input a string
+
+# reverse the string and removes the space characters at the beginning of the string
+
+# print the result
