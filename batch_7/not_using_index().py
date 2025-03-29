f8057c4f37bbab34d0c81e4edadceca0f3721a23
Kurt <dilanmarinas@gmail.com>
2025-03-29 15:28:19 +0800

    start prog 9 and pseudocode

---
+++ batch_7/not_using_index().py
@@ -0,0 +1,7 @@
+# Prog09. index() return the first location of the function parameter in the string. Create a program that do the same functionality without using index() function.
+
+# prompt the user to input a string
+
+# check the location of the parameter in the string
+
+# print the result
