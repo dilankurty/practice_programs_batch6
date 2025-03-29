1ae688600892e96dd3bb775b6889fb690166a43c
Kurt <dilanmarinas@gmail.com>
2025-03-29 14:53:44 +0800

    start prog 6 and pseudocode

---
+++ batch_7/not_using_rjust().py
@@ -0,0 +1,7 @@
+# Prog06. rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using rjust() function.
+
+# prompt the user to input a multiple line string
+
+# add left spaces to the string
+
+# print the result
