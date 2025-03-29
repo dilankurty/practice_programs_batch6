ff94f32015c95613aea70a82ad01cf1dc43ec6b5
Kurt <dilanmarinas@gmail.com>
2025-03-29 14:38:23 +0800

    start prog 5 and pseudocode

---
+++ batch_7/not_using_startswith().py
@@ -0,0 +1,7 @@
+# Prog05. startswith() check if the string beginning part matches the function parameter. Create a program that do the same functionality without using startswith() function.
+
+# prompt the user to input a string
+
+# check if the beginning matches the parameter
+
+# print the result
