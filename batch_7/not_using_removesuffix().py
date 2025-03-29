3f7cc3833e4147e6cdf8620e94505bdf2087b891
Kurt <dilanmarinas@gmail.com>
2025-03-29 14:13:27 +0800

    added input and lower it to ensure if the parameter is used

--- batch_7/not_using_removesuffix().py
+++ batch_7/not_using_removesuffix().py
@@ -1,6 +1,7 @@
 # Prog02. removesuffix() remove the characters at the end of the string that matches the function parameter. Create a program that do the same functionality without using removesuffix() function.
 
 # prompt the user to input a string
+string = input("Input a string: ").lower()
 
 # check if the suffix is used and remove it from the string
 
