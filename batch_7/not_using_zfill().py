6ba4d0b06a5c95a1d0a4636b21d5b42a71863390
Kurt <dilanmarinas@gmail.com>
2025-03-29 15:09:44 +0800

    added input

--- batch_7/not_using_zfill().py
+++ batch_7/not_using_zfill().py
@@ -1,7 +1,7 @@
 # Prog07. zfill() add zero characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using zfill() function.
 
 # prompt the user to input a string
-
+string = input("Input a string: ")
 
 # add zero at the beginning of the string (if the string is less than 5 characters)
 
